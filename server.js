8df67274d05139dad6467d742d6eadfe39fd46a1
codetective <iaminiodu@gmail.com>
2020-08-11 21:40:46 -0700

    route update

--- server.js
+++ server.js
@@ -1,184 +1,218 @@
-const express = require("express");
-const app = express();
-const port = process.env.PORT || 3000;
-const stockModel = require("./stock");
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 3000
+const stockModel = require('./stock')
+const userModel = require('./user')
 
 //connect database
-const mongoose = require("mongoose");
-mongoose.connect(process.env.MONGO_URI || "mongodb://localhost/shop", {
+const mongoose = require('mongoose')
+mongoose.connect(process.env.MONGO_URI || 'mongodb://localhost/shop', {
   useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+  useUnifiedTopology: true
+})
 
-const db = mongoose.connection;
-db.once("open", function () {
-  console.log("database is connected");
-});
-db.on("error", (e) => {
-  console.log(e);
-});
-// db.collections.stocks.drop(() => {
-//   console.log('dropped');
-// })
+const db = mongoose.connection
+db.once('open', function () {
+  console.log('database is connected')
+})
+db.on('error', e => {
+  console.log(e)
+})
 
-const cors = require("cors");
-const stock = require("./stock");
-app.use(cors());
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+const cors = require('cors')
+const stock = require('./stock')
+app.use(cors())
 
-app.get("/", function (req, res) {
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+app.get('/', function (req, res) {
   stockModel.find((err, stocks) => {
     if (err) {
       res.json({
-        error: err.message,
-      });
+        error: err.message
+      })
     } else {
       res.json({
-        stocks,
-      });
+        stocks
+      })
     }
-  });
-});
+  })
+})
 
-//adding stock
-app.post("/", function (req, res) {
-  if (req.body.name === undefined || "" || req.body.qty === undefined || "") {
+app.post('/login', function (req, res) {
+  if (
+    req.body.name === undefined ||
+    '' ||
+    req.body.password === undefined ||
+    ''
+  ) {
     res.json({
-      msg: "form data incomplete",
-    });
+      msg: 'form data incomplete'
+    })
+  } else {
+    try {
+      // userModel.findOne({ name: req.body.name }).then(doc => {
+      //   if (doc.name !== req.body.name && doc.password !== req.body.password || doc.name !== req.body.name || doc.password !== req.body.password) {
+      //     res.json({
+      //       error: 'invalid user'
+      //     })
+      //   } else {
+      //     res.json({
+      //       name: doc.name,
+      //       msg: 'user found'
+      //     })
+      //   }
+      // })
+      userModel.create({name: req.body.name, password: req.body.password})
+        .then((docs) => {
+          res.json({
+          docs
+        })
+      })
+    } catch (e) {
+      console.log(e)
+    }
+  }
+})
+app.post('/', function (req, res) {
+  if (req.body.name === undefined || '' || req.body.qty === undefined || '') {
+    res.json({
+      msg: 'form data incomplete'
+    })
   } else {
     const newStock = {
       name: req.body.name.toLowerCase().toString(),
-      qty: req.body.qty,
-    };
+      qty: req.body.qty
+    }
     stockModel
       .findOne({
-        name: newStock.name,
+        name: newStock.name
       })
-      .then((stock) => {
+      .then(stock => {
         if (!stock) {
           stockModel
             .create(newStock)
-            .then((doc) => {
+            .then(doc => {
               res.json({
-                status: "success",
-                message: "stock added sucessfully",
-              });
+                status: 'success',
+                message: 'stock added sucessfully'
+              })
             })
-            .catch((err) => {
+            .catch(err => {
               res.json({
-                error: err.message,
-              });
-            });
+                error: err.message
+              })
+            })
         } else {
           res.json({
-            msg: "Stock already exists",
-          });
+            msg: 'Stock already exists'
+          })
         }
       })
-      .catch((err) => {
+      .catch(err => {
         res.send({
           err,
-          msg: err,
-        });
-      });
+          msg: err
+        })
+      })
   }
-});
+})
 
-//adding sale
-app.post("/sale", async function (req, res) {
-  let resp = [];
-  let errm = [];
-  let count = 0;
-  const result = await req.body.forEach((item) => {
-    count++;
+app.post('/sale', async function (req, res) {
+  let resp = []
+  let errm = []
+  let count = 0
+  const result = await req.body.forEach(item => {
+    count++
     stockModel
       .findOne({ name: item.name.toLowerCase().toString() })
-      .then((doc) => {
+      .then(doc => {
         if (!doc) {
           res.json({
-            status: "error",
-            msg: "Stocks invalid",
-          });
+            status: 'error',
+            msg: 'Stocks invalid'
+          })
         } else {
-          doc.qty = doc.qty - item.qty;
+          doc.qty = doc.qty - item.qty
           doc
             .save()
-            .then((dc) => {
-              resp.push("saved " + dc.name +' :' + dc.qty);
+            .then(dc => {
+              resp.push('saved ' + dc.name + ' :' + dc.qty)
               if (count === req.body.length) {
                 res.json({
                   status: 'success',
-                  info : JSON.stringify(resp)
+                  info: JSON.stringify(resp)
                 })
               }
             })
-            .catch((err) => {
-              errm.push("error occured on item :" + count);
-              if (count === req.body.length && errm.length === req.body.length) {
-                 res.json({
-                   status: "error",
-                   info: JSON.stringify(errm),
-                 });
+            .catch(err => {
+              errm.push('error occured on item :' + count)
+              if (
+                count === req.body.length &&
+                errm.length === req.body.length
+              ) {
+                res.json({
+                  status: 'error',
+                  info: JSON.stringify(errm)
+                })
               } else {
-                return;
-               }
-            });
+                return
+              }
+            })
         }
       })
-      .catch((err) => {
+      .catch(err => {
         res.json({
           status: 'error',
           msg: 'cannot update sales'
         })
-      });
-  });
-});
+      })
+  })
+})
 
-app.delete("/", function (req, res) {
+app.delete('/', function (req, res) {
   stockModel
     .findOneAndDelete({
-      name: req.body.name,
-    })
-    .then((doc) => {
-      if (doc === null) {
-        res.json({
-          msg: "document does not exist",
-        });
-      } else {
-        res.status(200).json({
-          doc,
-        });
-      }
-    });
-});
-
-app.put("/", function (req, res) {
-  stockModel
-    .findOne({
       name: req.body.name
     })
-    .then((doc) => {
-      doc.updateOne({qty : req.body.qty}).then((dck) => {
-        console.log(dck);
+    .then(doc => {
+      if (doc === null) {
+        res.json({
+          msg: 'document does not exist'
+        })
+      } else {
+        res.status(200).json({
+          doc
+        })
+      }
+    })
+})
+
+app.put('/', function (req, res) {
+  stockModel
+    .findOne({
+      name: req.body.name.toLowerCase()
+    })
+    .then(doc => {
+      doc.updateOne({ qty: req.body.qty }).then(dck => {
         res.json({
           status: 'success',
           message: 'updated'
-     })
-   })
+        })
+      })
     })
-    .catch((err) => {
+    .catch(err => {
       res.json({
-        status: "failed",
-        error: 'stock does not exist in database',
-      });
-    });
-});
+        status: 'failed',
+        error: 'stock does not exist in database'
+      })
+    })
+})
 
-app.get("*", function (req, res) {
-  res.status(404).send({ error: "page does not exist. Winter is Coming!!" });
-});
+app.get('*', function (req, res) {
+  res.status(404).send({ error: 'page does not exist. Winter is Coming!!' })
+})
 
-app.listen(port, () => console.log(`listening at http://localhost:${port}`));
+app.listen(port, () => console.log(`listening at http://localhost:${port}`))
